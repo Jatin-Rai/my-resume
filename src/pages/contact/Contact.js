aeadfb8a99f718c34717e7a0eff8825e833ec64c
jatin-rai <jatinrai258@gmail.com>
2022-10-08 09:51:40 +0530

    changed contact email address in contact page

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -22,7 +22,7 @@ const Contact = () => {
                         <p className="text-gray-700 text-base mb-4">
                             {email}
                         </p>
-                        <a className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out cursor-pointer" href='mailto:raij168@gmail.com'>Open Gmail</a>
+                        <a className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out cursor-pointer" href='mailto:jatinrai258@gmail.com'>Open Gmail</a>
                     </div>
                 </div>
             </div>
